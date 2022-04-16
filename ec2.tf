b8475074de3832d1d013ede2f806282376f01be6
awstesting1019 <103477903+awstesting1019@users.noreply.github.com>
2022-04-16 03:20:18 -0500

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "AKIAVYSNLTQT47EI4LHI"
+  secret_key = "kpxmAVSos5EBzWapGQpPFqvsVoVstiLxmD4dHw8o"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-03ededff12e34e59e"
+  instance_type = "t2.micro"
+}
