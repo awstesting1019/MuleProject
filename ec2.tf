fbe6b3f97908c5a29184088a548dc5e2b815f2ef
awstesting1019 <103477903+awstesting1019@users.noreply.github.com>
2022-04-16 12:51:00 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
-provider "aws" {
-  access_key = "AKIAVYSNLTQT47EI4LHI"
-  secret_key = "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"
-  region     = "us-east-1"
-}
+#provider "aws" {
+#  access_key = "AKIAVYSNLTQT47EI4LHI"
+#  secret_key = "kpxmAVSos5EBzWapGQpPFqvsVoVstiLxmD4dHw8o"
+#  region     = "us-east-1"
+#}
 
-resource "aws_instance" "example" {
-  ami           = "ami-03ededff12e34e59e"
-  instance_type = "t2.micro"
-  key_name = "MuleRuntime"
-}
+#resource "aws_instance" "example" {
+#  ami           = "ami-03ededff12e34e59e"
+#  instance_type = "t2.micro"
+#  key_name = "MuleRuntime"
+#}
