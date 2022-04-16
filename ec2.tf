6e8c7a3e15e3a5b9ec3854bb31cdab89b68932d4
awstesting1019 <103477903+awstesting1019@users.noreply.github.com>
2022-04-16 13:19:31 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
-provider "aws" {
-  access_key = "AKIAVYSNLTQT47EI4LHI"
-  secret_key = "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"
-  region     = "us-east-1"
-}
+#provider "aws" {
+#  access_key = "AKIAVYSNLTQT47EI4LHI"
+#  secret_key = "kpxmAVSos5EBzWapGQpPFqvsVoVstiLxmD4dHw8o"
+#  region     = "us-east-1"
+#}
 
-resource "aws_instance" "example" {
-  ami           = "ami-03ededff12e34e59e"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-}
+#resource "aws_instance" "example" {
+# ami           = "ami-03ededff12e34e59e"
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+#}
